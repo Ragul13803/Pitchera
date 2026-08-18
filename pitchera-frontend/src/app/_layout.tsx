ebeccdeab7d3a8b30b0421ec8c65b6cc1a323184
RagulSuresh1382002 <ragulsuresh13802@gmail.com>
2026-08-18 15:49:22 +0530

    tailwind setup added

--- pitchera-frontend/src/app/_layout.tsx
+++ pitchera-frontend/src/app/_layout.tsx
@@ -2,6 +2,7 @@ import { AuthProvider } from "@/context/AuthContext";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { Stack } from "expo-router";
 import * as WebBrowser from 'expo-web-browser';
+import "../global.css";
 
 WebBrowser.maybeCompleteAuthSession();
 
